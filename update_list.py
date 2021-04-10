f940722310067574cd1c6af0c776027f4a96d4d9
U-dmlmont-PC\dmlmont <dml.mont@gmail.com>
2021-04-09 20:35:47 -0500

    Updated files

---
+++ update_list.py
@@ -0,0 +1,31 @@
+import requests
+import json
+# Request ee catalog
+eeCatalog = requests.get('https://earthengine-stac.storage.googleapis.com/catalog/catalog.json').json()
+# Get the datasets
+eeDict = dict()
+for dataset in eeCatalog['links']:
+    if dataset['rel'] == 'child':
+        response = requests.get(dataset['href'])
+        if response.status_code == 200:
+            dataset = requests.get(dataset['href']).json()
+            if dataset['gee:type'] in ['image','image_collection']:
+                if 'eo:bands' in list(dataset['properties'].keys()):
+                    bands = dataset['properties']['eo:bands']
+                    if len(bands) > 0:
+                        bandsDict = dict()
+                        for band in bands:
+                            name = band['name']
+                            if 'gee:scale' in list(band.keys()):
+                                scale = band['gee:scale']
+                            else:
+                                scale = 1.0
+                            if 'gee:offset' in list(band.keys()):
+                                offset = band['gee:offset']
+                            else:
+                                offset = 0.0
+                            bandsDict[name] = {'scale':scale, 'offset':offset}
+                        eeDict[dataset['id']] = bandsDict
+# Save the list as a json file
+with open('ee-catalog-scale-offset-parameters.json','w') as fp:
+    json.dump(eeDict, fp, indent = 4, sort_keys = True)
